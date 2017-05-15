3c0a8d0b03cf9eabd932ae774c7a6ddf9d6e207c
Gloria Budiman <wahyu.g@gmail.com>
2017-05-15 16:53:19 -0700

    Menu layout update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root                         to: 'pages#index'
   get      '/paper',           to: 'pages#paper'
-  get      '/schedule',        to: 'pages#schedule'
-  get      '/sponsor',         to: 'pages#sponsor'
+  get      '/important_dates',        to: 'pages#important_dates'
+  get      '/organizing_committee',         to: 'pages#organizing_committee'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
