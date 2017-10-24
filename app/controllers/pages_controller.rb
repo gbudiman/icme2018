ca32974c162fc497b46cd845fc4009b3c4c3617e
gbudiman <wahyu.g@gmail.com>
2017-10-24 07:06:28 -0700

    Add carousel on splash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,20 @@
 class PagesController < ApplicationController
   def index
+  	@carousels = {
+  		'Henrik Christensen' => ['IEEE/AAAS Fellow', 'Qualcomm Chancellor\'s Chair, UC San Diego, USA', 'Keynote Speaker'],
+  		'Gomila Cristina' => ['CTO, Technicolor, France', 'Keynote Speaker'],
+  		'Shrikanth Narayanan' => ['IEEE/AAAS/ASA/ISCA/NAI Fellow', 'Niki & C. L. Max Nikias Chair, University of Southern California, USA', 'Keynote Speaker'],
+  		'Ali Begen' => ['Tutorial Speaker', 'Delivering Traditional and Omnidirectional Media'],
+  		'Liangping Ma' => ['Tutorial Speaker', 'Delivering Traditional and Omnidirectional Media'],
+  		'Christian Timmerer' => ['Tutorial Speaker', 'Delivering Traditional and Omnidirectional Media'],
+  		'Kari Pulli' => ['Tutorial Speaker', 'Interactive Augmented Reality with Meta 2'],
+  		'Charles Niu' => ['Tutorial Speaker', 'Interactive Augmented Reality with Meta 2'],
+  		'Paulo Jansen' => ['Tutorial Speaker', 'Interactive Augmented Reality with Meta 2'],
+  		'Jens-Rainer Ohm' => ['Tutorial Speaker', 'Interactive Augmented Reality with Meta 2'],
+  		'Mathias Wien' => ['Tutorial Speaker', 'Interactive Augmented Reality with Meta 2'],
+  		'Tao Mei' => ['Tutorial Speaker', 'Multimedia and Language: Bridging Multimedia and Natural Language with Deep Learning'],
+  		'Jiebo Luo' => ['Tutorial Speaker', 'Multimedia and Language: Bridging Multimedia and Natural Language with Deep Learning']
+  	}
     render 'index'
   end
 end
--- public/assets/application-0104827e5d0cd8c1fd4f2decfac0ae0b70df79230e50dac3a49e6496a691570a.js
+++ public/assets/application-0104827e5d0cd8c1fd4f2decfac0ae0b70df79230e50dac3a49e6496a691570a.js
@@ -14585,18 +14585,40 @@ var layout_helper = (function() {
     venuemap.css('width', parent_width + 'px');
   }
 
+  var adjust_carousels = function() {
+    var width = $('#landing-carousel').width();
+    var slice = 120;
+    var half = slice / 2;
+    $('img.carousel')
+      .css('width', (width - slice) + 'px')
+      .css('margin-left', half + 'px')
+      .css('margin-right', half + 'px');
+
+    // $('.carousel-landing-caption')
+    //   .css('margin-left', half + 'px')
+    //   .css('margin-right', half + 'px');
+
+    $('.carousel-indicators.carousel-landing')
+      .css('top', (width - slice - 32) + 'px');
+
+    $('#jumbotron-title').parent().css('min-height', width + 'px');
+  }
+
   return {
     adjust_background: adjust_background,
+    adjust_carousels: adjust_carousels,
     adjust_map: adjust_map
   }
 })()
 
 $(function() {
   layout_helper.adjust_background();
+  layout_helper.adjust_carousels();
 
   $(window).resize(function() {
     layout_helper.adjust_background();
     layout_helper.adjust_map();
+    layout_helper.adjust_carousels();
   })
 })
 ;
