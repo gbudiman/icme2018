261c636f74ef190672da11d3a666d8b8dafcabe5
Gloria Budiman <wahyu.g@gmail.com>
2018-05-10 18:59:07 -0700

    Get unregistered

--- app/models/visa.rb
+++ app/models/visa.rb
@@ -1,3 +1,9 @@
+class String
+	def latinize
+		return self.gsub(/[^A-Za-z ]/, '').gsub(/\s+/, ' ')
+	end
+end
+
 class Visa < ApplicationRecord
 	# @@lut = {
 	# 	'test test' => { name: 'Test Test', 
@@ -16,6 +22,8 @@ class Visa < ApplicationRecord
 	@@lut = nil
 	@@registereds = nil
 	@@r_original_name = nil
+	@@paper_authors = nil
+	@@paper_emails = nil
 
 	def self.get key
 		if @@lut == nil then Visa.preprocess end
@@ -43,11 +51,13 @@ class Visa < ApplicationRecord
 	end
 
 	def self.preprocess
+		@@paper_authors = {}
+		@@paper_emails = {}
 		papers = IO.read(Rails.root.join('app', 'models', 'papers.txt')).split(/\n/)
+		emails = IO.read(Rails.root.join('app', 'models', 'emails.txt')).split(/\n/)
 		author_groups = IO.read(Rails.root.join('app', 'models', 'authors.txt')).split(/\n/)
 
 		vlut = {}
-		#ap papers
 		authors = []
 
 		author_groups.each do |group|
@@ -56,9 +66,13 @@ class Visa < ApplicationRecord
 			end)
 		end
 
+
 		authors.each_with_index do |author_groups, i|
 			paper = papers[i]
+			@@paper_authors[paper] = []
+			@@paper_emails[paper] = emails[i]
 			author_groups.each do |author|
+				@@paper_authors[paper].push(author.unicode_normalize.latinize.downcase)
 				vlut[author.downcase] = { name: author,
 				                          paper_title: paper,
 				                          authors: author_groups.join(', ') }
@@ -93,5 +107,60 @@ class Visa < ApplicationRecord
 		end
 
 		IO.write(Rails.root.join('app', 'models', 'email_unrolled.txt'), emails.keys.join("\n"))
+	end
+
+
+	def self.get_unregistered_papers
+		preprocess
+		slurp_cvent_data
+		
+		csv_papers = []
+		csv_emails = []
+		regs = {}
+		reg_count = 0
+		noreg_count = 0
+		@@registereds.each do |k, v|
+			regs[k.latinize] = true
+		end
+
+		@@paper_authors.each do |paper, authors|
+			
+			has_one_register = false
+			buffer = []
+			authors.each do |author|
+				splits = author.split(/\s+/)
+
+				if splits.length == 2
+					alt = "#{splits[1]} #{splits[0]}"
+					if regs[alt] != nil
+						has_one_register = true
+					end
+				end
+
+				if regs[author] != nil
+					has_one_register = true
+				end
+
+				buffer.push "> [#{regs[author] != nil ? 'T' : 'F'}] #{author}"
+			end
+
+			if not has_one_register
+				#puts paper
+				#puts buffer.join("\n")
+				noreg_count = noreg_count + 1
+				#puts @@paper_emails[paper]
+				#csv.push([paper, @@paper_emails[paper]])
+				csv_papers.push(paper)
+				csv_emails.push(@@paper_emails[paper])
+			else
+				reg_count = reg_count + 1
+			end
+		end
+
+		ap reg_count
+		ap noreg_count
+		puts csv_papers.join("\n")
+		puts csv_emails.join("\n")
+		return nil
 	end
 end
